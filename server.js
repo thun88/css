c61fc6e32582042f051b6f2ca28429667876d780
Christopher Lepore <chris.lepore@infor.com>
2017-02-16 11:50:12 -0500

    serve styleguide

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
+const ROOT = __dirname + "/" + "www/";
 
 // App
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('www'));
 
 app.get('/', function (req, res) {
-  res.sendFile( __dirname + "/" + "index.html" );
+  res.sendFile( ROOT + "index.html" );
 });
 
 app.listen(PORT);
