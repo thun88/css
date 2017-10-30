9f8516e43be65f051515551ea2a58a920e6da81a
Christopher Lepore <chris.lepore@infor.com>
2017-10-30 15:18:15 -0400

    Convert yaml to json

--- gulp/tasks/json-yaml-compile.js
+++ gulp/tasks/json-yaml-compile.js
@@ -2,9 +2,12 @@
 //   Copies src/*.yml files to pubish
 // -------------------------------------
 
+var yaml = require('gulp-yaml');
+
 module.exports = (gulp, paths) => {
-  gulp.task('src:yaml:copy', () => {
+  gulp.task('json:yaml:compile', () => {
     return gulp.src(`${paths.src.root}/*.yaml`)
+      .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
       .pipe(gulp.dest(`${paths.dist}`));
   });
 }
--- gulpfile.js
+++ gulpfile.js
@@ -54,13 +54,13 @@ require(`${paths.tasks}/clean.js`)(gulp, paths);
 require(`${paths.tasks}/css-lint.js`)(gulp, paths);
 require(`${paths.tasks}/json-js-compile.js`)(gulp, paths, publishDocObj);
 require(`${paths.tasks}/json-md-compile.js`)(gulp, paths, publishDocObj);
+require(`${paths.tasks}/json-yaml-compile.js`)(gulp, paths);
 require(`${paths.tasks}/deploy.js`)(gulp, paths);
 require(`${paths.tasks}/serve.js`)(gulp, paths);
 require(`${paths.tasks}/site-css-compile.js`)(gulp, paths, postCssPlugins);
 require(`${paths.tasks}/site-html-compile.js`)(gulp, paths, postCssPlugins, arrOfIcons, compiledSvgHtml);
 require(`${paths.tasks}/src-css-compile.js`)(gulp, paths, postCssPlugins);
 require(`${paths.tasks}/src-js-compile.js`)(gulp, paths);
-require(`${paths.tasks}/src-yaml-copy.js`)(gulp, paths);
 require(`${paths.tasks}/svg-optimize.js`)(gulp, paths, arrOfIcons);
 require(`${paths.tasks}/svg-store.js`)(gulp, paths, arrOfIcons);
 
